d36a383ba98296026eea472a0560ec9f1522dcf6
unknown <mrhdigital@yahoo.com>
2019-10-23 21:29:32 -0500

    function bubbleSort works

--- assets/javascript/bubbleSort.js
+++ assets/javascript/bubbleSort.js
@@ -1,13 +1,34 @@
 //alert("Hello");
 
-var arraySort = [50, 45,23,2];
-var SortArray = "";
-   
-function bubbleSort (arr){
+var arraySort = [45, 50, 23, 2];
+var SortArray = [];
+
+function bubbleSort(arr) {
+
+    var sorted = false
+    while (!sorted) {
+        sorted = true;
+        // // console.log(sorted);
+         for (var i = 0; i < 3; i++) {
+            // console.log(i);
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+                // console.log("for");
+                var temp = arr[i];
+                // console.log(temp);
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                // console.log(arr[i+1]);
+
+            }
+
+        }
+        
+    }
     return arr;
 }
 
 SortArray = bubbleSort(arraySort);
 console.log(SortArray);
-document.getElementById("unsortedArray").innerHTML = arraySort;
+document.querySelector("#unsortedArray").innerText = arraySort;
 document.getElementById("sortedArray").innerHTML = SortArray;
