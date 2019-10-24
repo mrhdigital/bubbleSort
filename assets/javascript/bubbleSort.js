40f1d5d90e4f947a0de21f6494f827e180efa389
unknown <mrhdigital@yahoo.com>
2019-10-23 19:32:50 -0500

    function bubbleSort is linked

--- assets/javascript/bubbleSort.js
+++ assets/javascript/bubbleSort.js
@@ -1,6 +1,13 @@
-alert("Hello");
+//alert("Hello");
 
 var arraySort = [50, 45,23,2];
+var SortArray = "";
+   
+function bubbleSort (arr){
+    return arr;
+}
 
-
+SortArray = bubbleSort(arraySort);
+console.log(SortArray);
 document.getElementById("unsortedArray").innerHTML = arraySort;
+document.getElementById("sortedArray").innerHTML = SortArray;
