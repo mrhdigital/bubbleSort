81ea03cb1802b0500520667745e5fb234d4bfa38
unknown <mrhdigital@yahoo.com>
2019-10-26 13:01:42 -0500

    add comments

--- assets/javascript/bubbleSort.js
+++ assets/javascript/bubbleSort.js
@@ -12,15 +12,17 @@ var ifCounter = 0;
 var forCounter = 0;
 // create function bubbleSort that accept array as parameter.
 function bubbleSort(arr) {
-
+// create var sorted that will act as a flag to let us know if our array has been completely sorted.
     var sorted = false
     while (!sorted) {
         sorted = true;
         whileCounter++;
         console.log(whileCounter);
+        // Loop through the array
         for (var i = 0; i < arr.length; i++) {
             forCounter++;
             // console.log(i);
+            // if the current element is larger than the next element, swap them and set sorted to "false"
             if (arr[i] > arr[i + 1]) {
                 sorted = false;
                 ifCounter++;
@@ -36,27 +38,28 @@ function bubbleSort(arr) {
         }
 
     }
-
+// If we looped through the array without having to swap anything, exit the while loop and return the array.
     return arr;
 
 }
 
 
-
-
+ 
+// Get a reference to the unsorted array button in he DOM, add a click event listener to it
 var startBtn = document.querySelector("#start");
 
 startBtn.addEventListener("click", function () {
-    
+    // insert the contents of the "arrayUnsortedCopy" into the span with id = #unsortedArray
     document.querySelector("#unsortedArray").innerText = arrayUnsortedCopy.join(", ");
 });
 
-
+// Get a reference to the sorted array button in he DOM, add a click event listener to it
 var sortBtn = document.querySelector("#sort");
 
 sortBtn.addEventListener("click", function () {
-    
+    // call function bubbleSort and pass arguement arrayUnsorted and store the result in a variable SortArray
     SortArray = bubbleSort(arrayUnsorted);
+    // insert the contents of the "arrayUnsorted" into the span with id = #sortedArray
     document.querySelector("#sortedArray").innerText = SortArray.join(", ");
     document.getElementById("whileCounter").innerHTML = whileCounter;
     document.getElementById("ifCounter").innerHTML = ifCounter;
